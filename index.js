0175456e6cc2399f0c564e6e9b704f12b725bb0f
BrandonW52 <157122603+BrandonW52@users.noreply.github.com>
2024-06-04 16:51:38 +0200

    Change to try...catch

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-fetch(
-  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=cyberpunk"
-)
-  .then((res) => res.json())
-  .then((data) => {
-    document.body.style.backgroundImage = `url(${data.urls.regular})`;
-    document.getElementById("author").textContent = `By: ${data.user.name}`;
-  })
-  .catch((err) => {
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1628329336337-8c33a8f08ec1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTc1MDQzODZ8&ixlib=rb-4.0.3&q=80&w=1080)`;
-    document.getElementById("author").textContent = `By: Lorenzo Herrera`;
-  });
+try {
+  const res = await fetch(
+    "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=cyberpunk"
+  );
+  const data = await res.json();
+  document.body.style.backgroundImage = `url(${data.urls.regular})`;
+  document.getElementById("author").textContent = `By: ${data.user.name}`;
+} catch (err) {
+  document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1628329336337-8c33a8f08ec1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTc1MDQzODZ8&ixlib=rb-4.0.3&q=80&w=1080)
+)`;
+  document.getElementById("author").textContent = `By: Lorenzo Herrera`;
+}
 
-fetch("https://api.coingecko.com/api/v3/coins/pepecoin-2")
-  .then((res) => {
-    if (!res.ok) {
-      throw Error("Something went wrong");
-    }
-    return res.json();
-  })
-  .then((data) => {
-    document.getElementById("crypto-top").innerHTML = `
+try {
+  const res = await fetch("https://api.coingecko.com/api/v3/coins/pepecoin-2");
+  if (!res.ok) {
+    throw Error("Something went wrong");
+  }
+  const data = await res.json();
+  document.getElementById("crypto-top").innerHTML = `
         <img src=${data.image.small} />
         <span>${data.name}</span>
     `;
-
-    document.getElementById("crypto").innerHTML += `
+  document.getElementById("crypto").innerHTML += `
             <p>🏷️: R ${data.market_data.current_price.zar}</p>
             <p>📈: R ${data.market_data.high_24h.zar}</p>
             <p>📉: R ${data.market_data.low_24h.zar}</p>
-        `;
-  })
-  .catch((err) => console.error(err));
+    `;
+} catch (err) {
+  console.error(err);
+}
 
 function getCurrentTime() {
   const date = new Date();
@@ -42,23 +40,22 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000);
 
-navigator.geolocation.getCurrentPosition((position) => {
-  fetch(
-    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
-  )
-    .then((res) => {
-      if (!res.ok) {
-        throw Error("Weather data not available");
-      }
-      return res.json();
-    })
-    .then((data) => {
-      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-      document.getElementById("weather").innerHTML = `
-                <img src=${iconUrl} />
-                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-                <p class="weather-city">${data.name}</p>
-            `;
-    })
-    .catch((err) => console.error(err));
+navigator.geolocation.getCurrentPosition(async (position) => {
+  try {
+    const res = await fetch(
+      `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
+    );
+    if (!res.ok) {
+      throw Error("Weather data not available");
+    }
+    const data = await res.json();
+    const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    document.getElementById("weather").innerHTML = `
+            <img src=${iconUrl} />
+            <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+            <p class="weather-city">${data.name}</p>
+        `;
+  } catch (err) {
+    console.error(err);
+  }
 });

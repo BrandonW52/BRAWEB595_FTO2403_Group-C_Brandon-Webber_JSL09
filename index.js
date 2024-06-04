632d17f77471656a1a93e1bc07c24267c7dbe3cd
BrandonW52 <157122603+BrandonW52@users.noreply.github.com>
2024-06-04 15:17:33 +0200

    Add coingeko api && display coin data

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=;hjksdf;kljsdfgl;kdsjfgljksdfglkjhsdfg")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        throw Error("I'm an error!")
-        // document.body.style.backgroundImage = `url(${data.urls.regular})`
-		// document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        console.log("Something went wrong! 😭")
-        // This is where I can handle the error
-        // Choose to use a default background image
-    })
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=cyberpunk"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.getElementById("author").textContent = `By: ${data.user.name}`;
+  })
+  .catch((err) => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1628329336337-8c33a8f08ec1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTc1MDQzODZ8&ixlib=rb-4.0.3&q=80&w=1080)`;
+    document.getElementById("author").textContent = `By: Lorenzo Herrera`;
+  });
 
+fetch("https://api.coingecko.com/api/v3/coins/pepecoin-2")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+    `;
+
+    document.getElementById("crypto").innerHTML += `
+            <p>🏷️: R ${data.market_data.current_price.zar}</p>
+            <p>📈: R ${data.market_data.high_24h.zar}</p>
+            <p>📉: R ${data.market_data.low_24h.zar}</p>
+        `;
+  })
+  .catch((err) => console.error(err));
